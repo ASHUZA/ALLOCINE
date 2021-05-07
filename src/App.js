54637c7a7645e20ddba1f754362a73dc3065c78d
Ashuza <ashuzakasumbepascal19@gmail.com>
2021-05-07 11:50:48 +0100

    arangement of movies profil

--- src/App.js
+++ src/App.js
@@ -17,13 +17,15 @@ import axios from 'axios';
 function App() {
 
 
+  const My_apikey = "2e352e05a7d8b0a12370c4ba41e55909";
+
   //INSERTION DES FILMS 
 
   const [dataMovies, setdataMovies] = useState([])
   useEffect(() => {
     const recuperationdata = async () => {
-      const resultatMovies = await axios("https://api.themoviedb.org/3/movie/popular?api_key=2e352e05a7d8b0a12370c4ba41e55909&language=en-US&page=1")
-   
+      const resultatMovies = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${My_apikey}&language=en-US&page=1`)
+
 
       setdataMovies(resultatMovies.data.results);
     }
@@ -37,14 +39,13 @@ function App() {
       matricule: movie.id,
       image_profil: movie.backdrop_path,
       titre: movie.title,
-      
+
       image_principal: movie.poster_path,
       resume: movie.overview
 
     };
 
   })
-  console.log(films)
 
 
   // INSERTION DES SERIES
@@ -52,8 +53,8 @@ function App() {
   const [dataTv, setdataTv] = useState([])
   useEffect(() => {
     const recuperationdataTv = async () => {
- //     const resultatMovies = await axios("https://api.themoviedb.org/3/movie/popular?api_key=2e352e05a7d8b0a12370c4ba41e55909&language=en-US&page=1")
-      const resultatTv = await axios("https://api.themoviedb.org/3/tv/popular?api_key=2e352e05a7d8b0a12370c4ba41e55909&language=en-US&page=1")
+      //     const resultatMovies = await axios("https://api.themoviedb.org/3/movie/popular?api_key=${My_apikey}&language=en-US&page=1")
+      const resultatTv = await axios(`https://api.themoviedb.org/3/tv/popular?api_key=${My_apikey}&language=en-US&page=1`)
 
 
       setdataTv(resultatTv.data.results);
@@ -69,108 +70,113 @@ function App() {
       image_profilTv: Tv.backdrop_path,
       titreTv: Tv.name,
       image_principalTv: Tv.poster_path,
-      resumeTv : Tv.overview
+      resumeTv: Tv.overview
 
     };
 
   })
-  console.log(Tvs)
 
 
-//insertion profil
 
-  const [dataTv, setdataTv] = useState([])
+  //insertion profil
+
+  const [dataProfil, setdataProfil] = useState([])
   useEffect(() => {
-    const recuperationdataTv = async () => {
- //     const resultatMovies = await axios("https://api.themoviedb.org/3/movie/popular?api_key=2e352e05a7d8b0a12370c4ba41e55909&language=en-US&page=1")
-      const resultatTv = await axios("https://api.themoviedb.org/3/tv/popular?api_key=2e352e05a7d8b0a12370c4ba41e55909&language=en-US&page=1")
+    const recuperationdataProfil = async () => {
+      //     const resultatMovies = await axios("https://api.themoviedb.org/3/movie/popular?api_key=${My_apikey}&language=en-US&page=1")
+      const resultatProfil = await axios(`https://api.themoviedb.org/3/movie/793723?api_key=${My_apikey}&language=en-US`)
 
+      setdataProfil(resultatProfil.data);
 
-      setdataTv(resultatTv.data.results);
+      // console.log(resultatProfil.data.original_title)
     }
-    recuperationdataTv()
+    recuperationdataProfil()
   }, [])
 
 
 
-  let Tvs = dataTv.map(Tv => {
-    return {
-      matriculeTv: Tv.id,
-      image_profilTv: Tv.backdrop_path,
-      titreTv: Tv.name,
-      image_principalTv: Tv.poster_path,
-      resumeTv : Tv.overview
 
-    };
+  // let Profil = dataProfil.map(Profil => {
+  //   return {
+  //     matriculeProfil: Profil.id,
+  //     image_profilProfil: Profil.backdrop_path,
+  //     titreProfil: Profil.name,
+  //     image_principalProfil: Profil.poster_path,
+  //     resumeProfil : Profil.overview
 
-  })
-  console.log(Tvs)
+  //   };
+
+  // })
+  // console.log(Tvs)
 
 
 
   return (
 
 
-
     <>
-
-
-      <Header />
-      <Cover />
+    <Header />
+      <Cover
+      src= {dataProfil.backdrop_path}
+      titre= {dataProfil.original_title}
+      release_date ={dataProfil.release_date}
+      />
       <SectionProfil />
       <SectionVideo />
 
-      
 
 
-<div className="containe d-flex align-items-center blockserie  flex-column">
 
-<div className="container bockcard">
-  <div className="section-title" data-aos="fade-up">
-    <h2> FILMS</h2>
-    <p> FILMS POPULAIRE </p>
-  </div>
-  <div className="row SectionMovies">
-    <div className="col-4 col-sm-4 col-sd-12 col-lg-12 col-xl-12 mb-5 d-flex justify-content-evenly flex-wrap cardMovies">
+      <div className="containe d-flex align-items-center blockserie  flex-column">
 
-
-      {films.map(function (element) {
-        return (
-          <Card
-            titre={element.titre}
-            resume = {element.resume}
-            src={`https://image.tmdb.org/t/p/original${element.image_principal}`}
-          />
-        );
-      })}
+        <div className="container bockcard">
+          <div className="section-title" data-aos="fade-up">
+            <h2> FILMS</h2>
+            <p> FILMS POPULAIRE </p>
+          </div>
+          <div className="row SectionMovies">
+            <div className="col-4 col-sm-4 col-sd-12 col-lg-12 col-xl-12 mb-5 d-flex justify-content-evenly flex-wrap cardMovies">
 
 
-    </div>
+              {films.map(function (element) {
+                return (
+                  <Card
 
-  </div>
-  
-</div>
-
-
-<div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-<div className="btn-group me-2" role="group" aria-label="First group">
-<button type="button" className="btn btn-primary">1</button>
-<button type="button" className="btn btn-primary">2</button>
-<button type="button" className="btn btn-primary">3</button>
-<button type="button" className="btn btn-primary">4</button>
-</div>
-<div className="btn-group me-2" role="group" aria-label="Second group">
-<button type="button" className="btn btn-primary">5</button>
-<button type="button" className="btn btn-primary">6</button>
-<button type="button" className="btn btn-primary">7</button>
-</div>
-<div className="btn-group" role="group" aria-label="Third group">
-<button type="button" className="btn btn-primary">8</button>
-</div>
-</div>
+                  
+                    titre={element.titre}
+                    resume={element.resume}
+                    src={`https://image.tmdb.org/t/p/original${element.image_principal}`}
+                  />
+                );
+              })}
 
 
-</div>
+            </div>
+
+          </div>
+
+        </div>
+
+
+        <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+          <div className="btn-group me-2" role="group" aria-label="First group">
+            <button type="button" className="btn btn-primary">1</button>
+            <button type="button" className="btn btn-primary">2</button>
+            <button type="button" className="btn btn-primary">3</button>
+            <button type="button" className="btn btn-primary">4</button>
+          </div>
+          <div className="btn-group me-2" role="group" aria-label="Second group">
+            <button type="button" className="btn btn-primary">5</button>
+            <button type="button" className="btn btn-primary">6</button>
+            <button type="button" className="btn btn-primary">7</button>
+          </div>
+          <div className="btn-group" role="group" aria-label="Third group">
+            <button type="button" className="btn btn-primary">8</button>
+          </div>
+        </div>
+
+
+      </div>
 
 
 
@@ -179,52 +185,52 @@ function App() {
 
       <div className="containe d-flex align-items-center blockserie  flex-column">
 
-      <div className="container bockcard">
-        <div className="section-title" data-aos="fade-up">
-          <h2> SERIES</h2>
-          <p> SERIES populaire </p>
-        </div>
-        <div className="row SectionMovies">
-          <div className="col-4 col-sm-4 col-sd-12 col-lg-12 col-xl-12 mb-5 d-flex justify-content-evenly flex-wrap cardMovies">
+        <div className="container bockcard">
+          <div className="section-title" data-aos="fade-up">
+            <h2> SERIES</h2>
+            <p> SERIES populaire </p>
+          </div>
+          <div className="row SectionMovies">
+            <div className="col-4 col-sm-4 col-sd-12 col-lg-12 col-xl-12 mb-5 d-flex justify-content-evenly flex-wrap cardMovies">
 
 
-            {Tvs.map(function (element) {
-              return (
-                <Card
-                  titre={element.titreTv}
-                  resume = {element.resumeTv}
-                  src={`https://image.tmdb.org/t/p/original${element.image_principalTv}`}
-                />
-              );
-            })}
+              {Tvs.map(function (element) {
+                return (
+                  <Card
+                    titre={element.titreTv}
+                    resume={element.resumeTv}
+                    src={`https://image.tmdb.org/t/p/original${element.image_principalTv}`}
+                  />
+                );
+              })}
 
+
+            </div>
 
           </div>
 
         </div>
-        
+
+
+        <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+          <div className="btn-group me-2" role="group" aria-label="First group">
+            <button type="button" className="btn btn-primary">1</button>
+            <button type="button" className="btn btn-primary">2</button>
+            <button type="button" className="btn btn-primary">3</button>
+            <button type="button" className="btn btn-primary">4</button>
+          </div>
+          <div className="btn-group me-2" role="group" aria-label="Second group">
+            <button type="button" className="btn btn-primary">5</button>
+            <button type="button" className="btn btn-primary">6</button>
+            <button type="button" className="btn btn-primary">7</button>
+          </div>
+          <div className="btn-group" role="group" aria-label="Third group">
+            <button type="button" className="btn btn-primary">8</button>
+          </div>
+        </div>
+
+
       </div>
-
-
-      <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-  <div className="btn-group me-2" role="group" aria-label="First group">
-    <button type="button" className="btn btn-primary">1</button>
-    <button type="button" className="btn btn-primary">2</button>
-    <button type="button" className="btn btn-primary">3</button>
-    <button type="button" className="btn btn-primary">4</button>
-  </div>
-  <div className="btn-group me-2" role="group" aria-label="Second group">
-    <button type="button" className="btn btn-primary">5</button>
-    <button type="button" className="btn btn-primary">6</button>
-    <button type="button" className="btn btn-primary">7</button>
-  </div>
-  <div className="btn-group" role="group" aria-label="Third group">
-    <button type="button" className="btn btn-primary">8</button>
-  </div>
-</div>
-
-
-</div>
 
 
       <SectionFooter />
--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,19 +1,23 @@
 import '../scss/Cover.scss';
 
-function Cover() {
 
-
+const Cover = (props) => {
     return (
+
         <>
 
 
 
 
 
-            <div id="intro" className="bg-image w-100 shadow-1-strong">
+            <div id="intro" className="bg-image w-100 shadow-1-strong secttioncover">
 
                 <img
-                    src="https://lumiere-a.akamaihd.net/v1/images/20cs_xmen_dark_phoenix_hero_banner_b26f8933.jpeg"
+                  //  src="https://lumiere-a.akamaihd.net/v1/images/20cs_xmen_dark_phoenix_hero_banner_b26f8933.jpeg"
+                  
+                    src={`https://image.tmdb.org/t/p/original${props.src}`}
+                 
+                    
                     className="d-block w-100"
                     alt="..."
                 />
@@ -29,8 +33,8 @@ function Cover() {
 
                     <div className="container-fluid covercontainer d-flex align-items-center justify-content-center text-center h-20">
                         <div className="justify-content-center text-white">
-                            <h1 className="mb-0">The New Mutants</h1>
-                            <h5 className="mb-2"> 1h 32min August 28, 2020 Horror, Originals, Thriller </h5>
+                            <h1 className="mb-0">{props.titre}</h1>
+                            <h5 className="mb-2"> 1h 32min {props.release_date}, 2020 Horror, Originals, Thriller </h5>
                             <a
                                 className="btn btn-outline-light btn-lg m-2"
                                 href="https://www.youtube.com/watch?v=c9B4TPnak1A"
